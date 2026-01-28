ccbc0c79259245442a2a3dc2425b4b6d92d58857
chloelammm <chloelhh04@gmail.com>
2026-01-28 18:32:45 +0800

    initial demo

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //base: '/math-genius/',           // ← ADD THIS LINE (your repo name)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
